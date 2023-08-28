f9fae5dae2786ae6d30848f47a4a2a89f68c4d2a
Laura <laha0005@stud.kea.dk>
2023-08-28 13:39:44 +0200

    js

--- productlist.js
+++ productlist.js
@@ -1,4 +1,4 @@
-fetch("https://kea-alt-del.dk/t7/api/products?limit=100")
+fetch("https://kea-alt-del.dk/t7/api/products")
     .then(res=>res.json())
     .then(showProduct)
 
@@ -9,15 +9,24 @@ function showProducts(products){
 function showProduct(product){
     //console.log(product)
     //fange template
-    const template = document.querySelector("smallProductTemplate").textContent;
+    const template = document.querySelector("#smallProductTemplate").textContent;
     //lav en kopi
     const copy = template.cloneNode(true);
     //ændre indhold
-
+    copy.querySelector("h3").textContent=product.productdisplayname;
     //appende
     document.querySelector("main").appendChild(copy);
 }
 /* 
+ <img src="https://kea-alt-del.dk/t7/images/webp/640/1163.webp" alt="Sahara Team India Fanwear Round Neck Jersey" />
+          <h3>Sahara Team India Fanwear Round Neck Jersey</h3>
+          <p class="subtle">Tshirts | Nike</p>
+          <p class="price"><span>Prev.</span> DKK 1595,-</p>
+          <div class="discounted">
+            <p>Now DKK 1560,-</p>
+            <p>-34%</p>
+          </div>
+          <a href="product.html">
 {
   "id": 1163,
   "gender": "Men",
