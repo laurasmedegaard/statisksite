91a14cbd9c3dc5da94a89ad2c03536e41bdc7dab
Laura <laha0005@stud.kea.dk>
2023-08-28 13:34:04 +0200

    js

--- productlist.js
+++ productlist.js
@@ -7,13 +7,30 @@ function showProducts(products){
     products.forEach(showProduct)
 }
 function showProduct(product){
-    console.log(product)
+    //console.log(product)
     //fange template
-
+    const template = document.querySelector("smallProductTemplate").textContent;
     //lav en kopi
-
-
+    const copy = template.cloneNode(true);
     //ændre indhold
 
     //appende
+    document.querySelector("main").appendChild(copy);
 }
+/* 
+{
+  "id": 1163,
+  "gender": "Men",
+  "category": "Apparel",
+  "subcategory": "Topwear",
+  "articletype": "Tshirts",
+  "season": "Summer",
+  "productionyear": 2011,
+  "usagetype": "Sports",
+  "productdisplayname": "Sahara Team India Fanwear Round Neck Jersey",
+  "price": 895,
+  "discount": null,
+  "brandname": "Nike",
+  "soldout": 0
+}
+*/
