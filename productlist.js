5398eb74aefa66d862ac1c6cbd3c7fc85553c1f9
Laura <laha0005@stud.kea.dk>
2023-08-29 12:57:21 +0200

    main

--- productlist.js
+++ productlist.js
@@ -1,4 +1,4 @@
-fetch("https://kea-alt-del.dk/t7/api/products?limit=10")
+fetch("https://kea-alt-del.dk/t7/api/products?limit=15")
     .then((res) => res.json())
     .then(showProducts);
 
@@ -14,14 +14,15 @@ function showProduct(product){
     const copy = template.cloneNode(true);
     //ændre indhold
     copy.querySelector("h3").textContent = product.productdisplayname;
+    copy.querySelector(".subtle").textContent = product.subcategory;
     copy.querySelector(".price").textContent = product.price;
+    copy.querySelector(".discounted").textContent = product.discount;
     copy.querySelector("img").src=`https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`
+
     if (product.soldout){
         //udsolgt
         copy.querySelector("article").classList.add("soldOut");
     }
-
-
     //appende
     document.querySelector("main").appendChild(copy);
 }
